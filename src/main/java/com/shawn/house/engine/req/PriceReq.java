f83efe8dd608f734ec033b2d53c871cb4cebc1a6
zengbingbing <zengbingbing@gyenno.com>
2018-06-07 13:52:42 +0800

    add Req

--- src/main/java/com/shawn/house/engine/req/PriceReq.java
+++ src/main/java/com/shawn/house/engine/req/PriceReq.java
@@ -1,9 +1,25 @@
 package com.shawn.house.engine.req;
 
+import lombok.Data;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.util.StringUtils;
+import java.io.IOException;
+
 /**
  * Created by shawn.zeng on 2018/6/4.
  */
+@Data
 public class PriceReq {
-    public static final String url= "http://202.103.39.36/chktest2.aspx";
+    public static final String url= "http://119.97.201.28:6081/chktest2.aspx";
 
+    private String gid;
+
+    public Document getDocument() throws IOException {
+        if(StringUtils.isEmpty(gid)){
+            return null;
+        }else {
+            return Jsoup.connect(url).data("gid",gid).timeout(60*1000).get();
+        }
+    }
 }
--- src/main/java/com/shawn/house/engine/req/RoomReq.java
+++ src/main/java/com/shawn/house/engine/req/RoomReq.java
@@ -1,5 +1,30 @@
 package com.shawn.house.engine.req;
 
+import lombok.Data;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.util.StringUtils;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
 public class RoomReq {
     public static final String url= "http://fgj.wuhan.gov.cn/zz_spfxmcx_fang.jspx";
+
+    private String dengJh;
+
+    private String houseDengJh;
+
+    Document getDocument() throws IOException {
+        if(StringUtils.isEmpty(dengJh)||StringUtils.isEmpty(houseDengJh)){
+            return null;
+        }else {
+            Map<String,String> data = new HashMap<>();
+            data.put("dengJh",dengJh);
+            data.put("houseDengJh",houseDengJh);
+            return Jsoup.connect(url).data(data).timeout(60*1000).get();
+        }
+    }
 }
